30eadfe9652f3f6f1bda2c20e707203745fa8285
syed asim <aasimshah8110@gmail.com>
2025-02-26 16:10:12 +0500

    npmstart

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
