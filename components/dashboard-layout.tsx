436531d25a66bf6df67e24a104002541870bf34e
syed asim <aasimshah8110@gmail.com>
2025-02-26 12:36:28 +0500

    initial commit

---
+++ components/dashboard-layout.tsx
@@ -0,0 +1,14 @@
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+import { Sidebar } from './sidebar';
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto p-8">{children}</main>
+    </div>
+  );
+}
---
+++ lib/services.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+const baseurl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const fetchAllVideos = async (lang: string) => {
+  try {
+    const response = await axios.get(baseurl + "/api/videos/all", {
+      headers: {
+        "Content-Type": "application/json",
+        language: lang,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteVideo = async (id: string) => {
+  try {
+    const response = await axios.delete(baseurl + `/api/videos/delete/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to delete video", error);
+    throw error;
+  }
+};
+
+export const addLanuage = async (data: any) => {
+  try {
+    const response = await axios.post(baseurl + "/api/languages/", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/// fetch all languages
+export const fetchAllLanguages = async () => {
+  try {
+    const response = await axios.get(baseurl + "/api/languages");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch languages", error);
+    throw error;
+  }
+};
+
+// delete language
+export const deleteLanguage = async (id: string) => {
+  try {
+    const response = await axios.delete(baseurl + `/api/languages/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to delete language", error);
+    throw error;
+  }
+};
+
+// get all stats
+
+export const fetchStats = async () => {
+  try {
+    const response = await axios.get(baseurl + "/api/videos/stats");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch stats", error);
+    throw error;
+  }
+};
